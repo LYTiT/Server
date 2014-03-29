9f975bd1124219508504af2724776ce6e5199a6d
llukyanov <leonid@lytit.com>
2014-03-29 23:18:39 +0530

    RailsAdmin to see entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 LytitServer::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+
   namespace :api, :defaults => {:format => 'json'}  do
     namespace :v1 do
       resources :users, only: :create
