52c587621555f9d29854e4888e39cfa60f03aa99
llukyanov <leonid@lytit.com>
2014-04-07 12:15:49 +0530

    User should have authentication_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   include Clearance::User
+
+  before_save :ensure_authentication_token
+
+  private
+
+  def ensure_authentication_token
+    self.authentication_token ||= SecureRandom.hex
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140329182033) do
+ActiveRecord::Schema.define(version: 20140407064413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,17 +33,28 @@ ActiveRecord::Schema.define(version: 20140329182033) do
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "users", force: true do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "email",                          null: false
-    t.string   "encrypted_password", limit: 128, null: false
-    t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.string   "email",                            null: false
+    t.string   "encrypted_password",   limit: 128, null: false
+    t.string   "confirmation_token",   limit: 128
+    t.string   "remember_token",       limit: 128, null: false
     t.string   "name"
+    t.string   "authentication_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
   add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+
+  create_table "venue_ratings", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "venue_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "venue_ratings", ["user_id"], name: "index_venue_ratings_on_user_id", using: :btree
+  add_index "venue_ratings", ["venue_id"], name: "index_venue_ratings_on_venue_id", using: :btree
 
   create_table "venues", force: true do |t|
     t.string   "name"
