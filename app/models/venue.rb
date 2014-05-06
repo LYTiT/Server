56a11966ed07049eeb31d61cb06bea681ebe61d0
Rohan Dey <rohandey@Rohans-MacBook-Air.local>
2014-05-06 21:08:37 +0530

    call spots_by_query instead of spots to get formatted_address

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -22,7 +22,9 @@ class Venue < ActiveRecord::Base
   def self.fetch_venues(q, latitude, longitude)
     list = []
     client = Venue.google_place_client
-    spots = client.spots(latitude, longitude, :name => q, :radius => 2000) #radius in meters
+  
+    #radius in meters
+    spots = client.spots_by_query(q, :radius => 2000, :lat => latitude, :lng => longitude) 
     spots.each do |spot|
       venue = Venue.where("google_place_key = ?", spot.id).first
       venue ||= Venue.new()
@@ -31,7 +33,7 @@ class Venue < ActiveRecord::Base
       venue.google_place_rating = spot.rating
       venue.latitude = spot.lat
       venue.longitude = spot.lng
-      venue.address = [spot.street_number, spot.street].join(' ')
+      venue.address = spot.formatted_address
       venue.city = spot.city
       venue.save 
       list << venue if venue.persisted?
@@ -46,3 +48,4 @@ class Venue < ActiveRecord::Base
 end
 
 #@client.spots(-33.8670522, 151.1957362, :radius => 100, :name => 'italian')
+#@client.spots_by_query('italian', :radius => 1000, :lat => '-33.8670522', :lng => '151.1957362')
