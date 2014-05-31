13114ade367f8e75d64c46663c0e213695e97313
llukyanov <leonid@lytit.com>
2014-06-01 04:41:00 +0530

    LYT-262 Slider/Hamburger Menu

---
+++ app/assets/javascripts/venue_manager.js
@@ -0,0 +1,18 @@
+var jPM = $.jPanelMenu({
+  menu: '#custom-menu-selector',
+  trigger: '#hamburger-menu-button',
+  openPosition: '170px',
+  afterOpen: function() {
+    $('#hamburger-menu-button').addClass('active');
+  },
+  afterClose: function() {
+    $('#hamburger-menu-button').removeClass('active');
+  }
+});
+
+$(function(){  
+  $('#hamburger-menu-button').click(function(){
+    jPM.on();  
+    $("#hamburger-menu-button").unbind('click');
+  })
+})
