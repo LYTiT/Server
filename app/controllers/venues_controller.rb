e6c3170d55141d11ba1a6cf28f47b1e6c8029d2a
llukyanov <leonid@lytit.com>
2014-06-01 05:19:22 +0530

    LYT-262 Slider/Hamburger Menu

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,12 +2,27 @@ class VenuesController < ApplicationController
 
   layout 'venue_manager'
 
+  before_filter :authorize
+  before_filter :authorized_manager?, only: [:show]
+
   def reported_comments
     @flagged_comments = FlaggedComment.order('id DESC')
   end
 
   def show
+    if @venue.to_param != params[:id] and params[:id].to_i == @venue.id
+      redirect_to venue_path(@venue)
+    end
+  end
+
+  private
+
+  def authorized_manager?
     @venue = Venue.find(params[:id])
+    unless @venue.user == current_user
+      #TODO: Something better could be done! This will currently display a 404 page.
+      raise ActionController::RoutingError.new("Not authorized to access this page")
+    end
   end
 
 end
