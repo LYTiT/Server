e6b41e95c0367a4da65d45c2df838b7779d6697b
llukyanov <leonid@lytit.com>
2014-06-02 22:56:24 +0530

    LYT-263 Venue Picker Drop Down Menu

--- app/assets/javascripts/venue_manager.js
+++ app/assets/javascripts/venue_manager.js
@@ -10,12 +10,20 @@ var jPM = $.jPanelMenu({
   }
 });
 
-$(function(){  
-  $('#hamburger-menu-button').click(function(){
-    jPM.on();  
-    $("#hamburger-menu-button").unbind('click');
-  });
-  $('.venue-selector a').click(function(){
-    return false;
-  });
+$(function(){
+  if($('body').hasClass('venue_manager')) {  
+    $('#hamburger-menu-button').click(function(){
+      jPM.on();  
+      $("#hamburger-menu-button").unbind('click');
+    });
+    $('.venue-selector a').click(function(){
+      $('.modal-backdrop').removeClass('hide').addClass('in');
+      $('.venue-selector-container ul.list-group').show();
+      return false;
+    });
+    $('.venue-selector-container ul.list-group li a, .modal-backdrop').click(function(){
+      $('.modal-backdrop').addClass('hide').removeClass('in');
+      $('.venue-selector-container ul.list-group').hide();
+    });
+  }
 })
