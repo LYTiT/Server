7239d3f7459682b67e63dadec18a847098e0b283
Rohan Dey <rohandey@Rohans-MacBook-Air.local>
2014-06-07 01:23:37 +0530

    LYT-319 default radius

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -56,6 +56,7 @@ class Venue < ActiveRecord::Base
   end
 
   def self.fetch_venues(fetch_type, q, latitude, longitude, meters = 2000)
+    meters ||= 2000
     list = []
     client = Venue.google_place_client
 
