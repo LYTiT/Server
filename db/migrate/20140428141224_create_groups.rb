01afab29fe9b401f0d1fa00e7ee8c69f38756258
llukyanov <leonid@lytit.com>
2014-07-13 18:28:23 -0400

    Update 20140428141224_create_groups.rb

--- db/migrate/20140428141224_create_groups.rb
+++ db/migrate/20140428141224_create_groups.rb
@@ -3,7 +3,6 @@ class CreateGroups < ActiveRecord::Migration
     create_table :groups do |t|
       t.string :name
       t.string :description
-      t.string :can_link_event #can link event
       t.boolean :is_public
       t.string :password
 
