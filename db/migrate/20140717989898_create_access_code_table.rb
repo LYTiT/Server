523145b6f55a6910da20a19772e4c76ed4a75265
Tim Davidson <timdavidson@Tim-Davidsons-MacBook-Pro.local>
2014-07-17 22:01:36 -0400

    fixed name error in db migrate create_access_code_table

--- db/migrate/20140717989898_create_access_code_table.rb
+++ db/migrate/20140717989898_create_access_code_table.rb
@@ -1,6 +1,6 @@
 #This creates a table called "AccessCodes",
 #table entries have two main fields, accesscode, kvalue, and created/modified timestamp fields
-class MakeAccessCodeTable < ActiveRecord::Migration
+class CreateAccessCodeTable < ActiveRecord::Migration
   def change
     create_table :AccessCodes do |t|
       t.string :accesscode
