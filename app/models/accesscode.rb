300c24421a1366a28b4482ef575d6250e8934e5e
Tim Davidson <timdavidson@Tim-Davidsons-MacBook-Pro.local>
2014-07-17 21:35:45 -0400

    Message

---
+++ app/models/accesscode.rb
@@ -0,0 +1,8 @@
+s just declares the accesscode object as a model
+#the model has two fields, accesscode and kvalue
+class AccessCode < ActiveRecord::Base
+
+  validates :accesscode, :kvalue
+
+end
+
