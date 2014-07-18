a9a7c944e6a2e18695859213d7410203a9fb8e72
Tim Davidson <timdavidson@Tim-Davidsons-MacBook-Pro.local>
2014-07-18 15:56:12 -0400

    changes to model access_code.rb

--- app/models/access_code.rb
+++ app/models/access_code.rb
@@ -1,6 +1,10 @@
 class AccessCode < ActiveRecord::Base
 
 #Check id the passed code existes and is not too long
+#Check if there is a k-value attached to the passed code
+
+  validates :accesscode, presence: true
+  validates :kvalue, presence: true
 
   validates_length_of :accesscode, :within => 1..5
 
