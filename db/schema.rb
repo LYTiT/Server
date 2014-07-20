b35807233a122296b43e4dcbe59b23664a203d2e
Tim Davidson <timdavidson@172-29-16-208.dynapool.nyu.edu>
2014-07-19 22:32:01 -0400

    rm

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140717989898) do
-
-ActiveRecord::Schema.define(version: 20140716143157) do
-
+ActiveRecord::Schema.define(version: 20140717000999) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "pg_stat_statements"
+
+  create_table "access_codes", force: true do |t|
+    t.string   "accesscode"
+    t.integer  "kvalue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comment_views", force: true do |t|
     t.integer  "venue_comment_id"
