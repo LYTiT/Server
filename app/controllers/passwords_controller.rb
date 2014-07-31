68c9bb24a5b3160c4cc3caf41b911d0dd41be8d3
llukyanov <leonid@lytit.com>
2014-08-01 03:10:17 +0530

    LYT-732 Password reset form typo, and formatting is wrong | Static Pages

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -6,7 +6,7 @@ class PasswordsController < Clearance::PasswordsController
     @user = find_user_for_update
 
     if @user.update_password password_reset_params
-      flash[:message] = "You're password has been successfully reset."
+      flash[:message] = "Your password has been successfully reset."
       redirect_to "/"
     else
       flash_failure_after_update
