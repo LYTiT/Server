585ba6db4d44f58448175b2d3dd867f05f3238d4
llukyanov <leonid@lytit.com>
2014-09-07 21:06:06 +0530

    LYT-768 Server side work for Push notifications

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -4,4 +4,15 @@ class Api::V1::NotificationsController < ApiBaseController
     @notifications = Notification.where(user_id: @user.id)
   end
 
+  def mark_as_read
+    notification = Notification.where(id: params[:notification_id], user_id: @user.id)
+    if notification.present?
+      notification[:read] = true
+      notification.save
+      render json: { success: true }
+    else
+      render json: { error: { code: ERROR_NOT_FOUND, messages: ["Notification/User not found"] } }, :status => :not_found
+    end
+  end
+
 end
--- app/controllers/api/v1/search.json.jbuilder
+++
@@ -1,7 +0,0 @@
-json.array! @venues do |v|
-  json.id v.id
-  json.name v.name
-  json.latitude v.latitude
-  json.longitude v.longitude
-  json.color_rating v.color_rating
-end
