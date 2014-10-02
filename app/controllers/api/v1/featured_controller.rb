97787337a82b4d295962ec923cdc4cd5ab7e7952
llukyanov <leonid@lytit.com>
2014-10-01 21:31:55 -0400

    Update featured_controller.rb
    SPOTLYT dates fix

--- app/controllers/api/v1/featured_controller.rb
+++ app/controllers/api/v1/featured_controller.rb
@@ -4,7 +4,9 @@ respond_to :json
   def today
   	selected_date = params[:featured_id]
   	now = Date.parse(selected_date)
-  	v = VenueComment.where("DATE(created_at) = DATE(?) AND media_type = ?", now, 'image')
+    now_1 = (now + 4.hour)
+    now_2 = (now + 28.hour)
+  	v = VenueComment.where("media_type = 'image' AND created_at <= ? AND created_at >= ?", now_2, now_1)
   	s = v.sort_by {|i| i.total_views}
   	@venue_comments = s.reverse.first(5)
   end
@@ -12,7 +14,9 @@ respond_to :json
   def allTime
   	selected_date = params[:featured_id]
   	now = Date.parse(selected_date)
-  	v = VenueComment.where("DATE(created_at) = DATE(?) AND media_type = ?", now, 'video')
+    now_1 = (now + 4.hour)
+    now_2 = (now + 28.hour)
+  	v = VenueComment.where("media_type = 'video' AND created_at <= ? AND created_at >= ?", now_2, now_1)
   	s = v.sort_by {|i| i.total_views}
   	@venue_comments = s.reverse.first(5)
   end
