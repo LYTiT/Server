e96377e2c5a7568971dbefc86344c602380be452
llukyanov <leonid@lytit.com>
2014-12-02 13:18:47 -0500

    Check 9

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -37,7 +37,10 @@ class Api::V1::VenuesController < ApiBaseController
     end
 
     if @user.venue_comments.count > 0
-      last_post = @user.venue_comments.order('id ASC').to_a.pop
+      #last_post = @user.venue_comments.order('id ASC').to_a.pop
+      #we pop off 5 Venue Comments just to be safe we are assigning parts in chronological order
+      last_posts = @user.venue_comments.order('id ASC').to_a.pop(5)
+      last_post = last_post.detect{|comment| comment.venue_id == 14002}
     end
 
     if last_post != nil and last_post.venue_id == 14002
