c4011877e9c6e73ab6922e0a24e6641b7e12424d
llukyanov <leonid@lytit.com>
2014-12-09 22:24:39 -0500

    Index naming fix

--- db/migrate/20141209195548_create_group_invitations.rb
+++ db/migrate/20141209195548_create_group_invitations.rb
@@ -7,9 +7,9 @@ class CreateGroupInvitations < ActiveRecord::Migration
 
       t.timestamps
     end
-    add_index :group_invitations, :group_id
+    add_index :group_invitations, :igroup_id
     add_index :group_invitations, :invited_id     
     add_index :group_invitations, :host_id
-    add_index :group_invitations, [:group_id, :invited_id], unique: true
+    add_index :group_invitations, [:igroup_id, :invited_id], unique: true
   end
 end
