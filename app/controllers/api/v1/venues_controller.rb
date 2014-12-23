6f9af944c5b5237d14e7967125b85ed585e69020
llukyanov <leonid@lytit.com>
2014-12-23 01:23:21 -0500

    Typo fix

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -67,6 +67,7 @@ class Api::V1::VenuesController < ApiBaseController
               post.username_private = @comment.username_private
               @comment = post
               completion = true
+            end
           end
 
           if completion == false #media part has not been uploaded yet
@@ -141,14 +142,6 @@ class Api::V1::VenuesController < ApiBaseController
       if (@comment.media_type == 'text' and @comment.consider? == 1) and update == false
         @user.update_lumens_after_text(@comment.id)
       end
-      #check to see if @Group message needs to be sent
-      if @comment.venue_id != 14002
-        for g_id in params[:at_ids]
-          receiving_group = Group.find_by_id(g_id)
-          receiving_group.at_group!(@comment) 
-        end
-      end
-
     end
   end
 
