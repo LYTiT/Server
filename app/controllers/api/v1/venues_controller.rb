b7de86db8132dc1542b497f0988e037fc62c55e8
llukyanov <leonid@lytit.com>
2014-12-24 01:07:28 -0500

    Debug 4

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -139,8 +139,8 @@ class Api::V1::VenuesController < ApiBaseController
 
       if @comment.venue_id != 14002
         for gid in params[:at_ids]
-          puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#{gid}"
-          receiving_group = Group.find(gid["user_id"])
+          puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#{gid["user_id"]}"
+          receiving_group = Group.find_by_id(gid["user_id"])
           receiving_group.at_group!(@comment)
         end
       end
