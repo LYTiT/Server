193d29b73e847c19a91aee2723a97bb702ec01be
llukyanov <leonid@lytit.com>
2014-12-26 22:30:06 -0500

    Groupfeed pagination fix 2

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -139,7 +139,8 @@ class Api::V1::GroupsController < ApiBaseController
 
   def get_groupfeed
     @group = Group.find(params[:group_id])
-    @comments = @group.groupfeed.to_a.page(params[:page]).per(5).order("created_at desc")
+    feed = @group.groupfeed
+    @comment = Kaminari.paginate_array(feed).page(params[:page]).per(5)
   end
 
   def report
