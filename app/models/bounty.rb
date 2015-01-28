00c0abe3530eaff632edeac4fd00794f0f4ef77b
llukyanov <leonid@lytit.com>
2015-01-27 20:31:21 -0500

    Added bounty validity check

--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -2,4 +2,14 @@ class Bounty < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :venue
 
+	def is_valid?
+		if self.expiration - self.created_at < 0
+			self.validity = false
+			save
+			return false
+		else
+			return true
+		end
+	end
+
 end
