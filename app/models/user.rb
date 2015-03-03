2c52883a858149eb34e44ab463b1b36a8a7e21b7
llukyanov <leonid@lytit.com>
2015-03-03 03:13:53 -0500

    Filtered out Bounty response moments from discovery feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -226,7 +226,7 @@ class User < ActiveRecord::Base
 
     surrounding_moment_requests = Bounty.joins(:venue).where('l_sphere = ?', current_sphere)
     surrounding_moment_request_responses = BountyClaim.joins(venue_comment: :venue).where('l_sphere = ?', current_sphere)
-    surrounding_moments = VenueComment.joins(:venue).where('l_sphere = ?', current_sphere)
+    surrounding_moments = VenueComment.joins(:venue).where('l_sphere = ?', current_sphere).where('bounty_claim_id IS NULL')
 
     feed = (surrounding_moment_requests + surrounding_moment_request_responses + surrounding_moments)
     surrounding_feed = feed.sort_by{|x,y| x.created_at}.reverse
