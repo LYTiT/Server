48d316167a1599fa09ddd3f8612a1430ab9bb324
llukyanov <leonid@lytit.com>
2015-03-30 02:09:02 -0400

    Test 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -252,7 +252,7 @@ class User < ActiveRecord::Base
     surrounding_moment_requests = Bounty.where("venue_id IN (#{venue_ids}) AND created_at >= ?", commencement)
     #surrounding_moment_request_responses = BountyClaim.joins(:venue_comment).where("venue_id IN (#{venue_ids})")
     surrounding_moment_request_responses = []
-    surrounding_moment_requests.each{|request| surrounding_moment_request_responses << request.bounty_claims.to_a}
+    surrounding_moment_requests.each{|request| surrounding_moment_request_responses << request.bounty_claims}
 
     feed = (surrounding_moment_requests << surrounding_moment_request_responses.flatten << surrounding_moments).flatten
     surrounding_feed = feed.sort_by{|x,y| x.created_at}.reverse
