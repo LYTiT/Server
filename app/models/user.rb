ec36038b28a781d80a7015e55d7b937e5f85848f
llukyanov <leonid@lytit.com>
2015-04-04 00:25:50 -0400

    Typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -190,11 +190,12 @@ class User < ActiveRecord::Base
     surrounding_feed = feed.sort_by{|x,y| x.created_at}.reverse
   end
 
+  #global feed
   def global_feed
     days_back = 5
     surrounding_moments = VenueComment.where('created_at >= ? AND bounty_claim_id IS NULL', (Time.now - days_back.days))
     surrounding_moment_requests = Bounty.where('created_at >= ?', (Time.now - days_back.days))
-    surrounding_moment_request_responses = BountyClaimy.where('created_at >= ?', (Time.now - days_back.days))
+    surrounding_moment_request_responses = BountyClaim.where('created_at >= ?', (Time.now - days_back.days))
 
     feed = (surrounding_moment_requests << surrounding_moment_request_responses << surrounding_moments).flatten
     surrounding_feed = feed.sort_by{|x,y| x.created_at}.reverse
