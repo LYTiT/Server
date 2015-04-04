11db47eafd6d902b3aeb358e820dce64d5b3d9e2
llukyanov <leonid@lytit.com>
2015-04-04 16:19:12 -0400

    Fix 7

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,6 +9,7 @@ class Api::V1::UsersController < ApiBaseController
       if @user.name[10] == @user.email[10] && @user.email.last(8) == "temp.com"
         @user.vendor_id = @user.name
         @user.name = "lyt_"+@user.id.to_s(16)
+        @user.save
       end
 
       sign_in @user
