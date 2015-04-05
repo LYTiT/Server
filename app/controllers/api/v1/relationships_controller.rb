db220bf069f91dcc6b1e49b38aa13e32ff965128
llukyanov <leonid@lytit.com>
2015-04-05 01:01:23 -0400

    view added to viewing

--- app/controllers/api/v1/relationships_controller.rb
+++ app/controllers/api/v1/relationships_controller.rb
@@ -4,12 +4,14 @@ class Api::V1::RelationshipsController < ApplicationController
   	@user = User.find_by_id(params[:relationship_id])
     current_user = User.find_by_authentication_token(params[:auth_token])
     current_user.follow!(@user)
+    render json: { success: true }
   end
 
   def destroy
   	@user = User.find(params[:relationship_id])
   	current_user = User.find_by_authentication_token(params[:auth_token])
     current_user.unfollow!(@user)
+    render json: { success: true }
   end
 
   def v_create
