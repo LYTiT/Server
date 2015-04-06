693f4e7eae03402a18a17f5d03f29ff0b67db803
llukyanov <leonid@lytit.com>
2015-04-06 18:07:23 -0400

    Fix 2

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,6 +1,6 @@
 class Coupon < ActiveRecord::Base
 	def Coupon.check_code(code, user)
-		retrieved_coupon = Coupon.where("code = ?", code)
+		retrieved_coupon = Coupon.where("code = ?", code).first
 		if retrieved_coupon != nil
 			claimers_count = CouponClaimer.where("coupon_id = ?", retrieved_coupon.id).count
 			if claimers_count >= retrieved_coupon.supply
@@ -8,7 +8,7 @@ class Coupon < ActiveRecord::Base
 			else
 				CouponClaimer.create!(coupon_id: retrieved_coupon.id, user_id: user.id)
 				user.update_columns(lumens: retrieved_coupon.lumen_gift)
-				resposne "Nice! + #{retrieved_coupon.lumen_gift} Lumens."
+				resposne "Nice! +#{retrieved_coupon.lumen_gift} Lumens."
 			end
 		else
 			response = "Code not valid."
