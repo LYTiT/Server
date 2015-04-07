a4bbcee6379a9d8dfdab4d199e29bc90d8945ce0
llukyanov <leonid@lytit.com>
2015-04-07 14:57:31 -0400

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,10 +25,10 @@ class UsersController < ApplicationController
     if @user
       @user.validate_email
       flash[:success] = "Email confirmed! You are entitled to the Lumen Game prize."
-      redirect_to root
+      #redirect_to root
     else
       flash[:error] = "Sorry. User does not exist"
-      redirect_to root
+      #redirect_to root
     end
 
   end
