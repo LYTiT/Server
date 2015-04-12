46f3de4a450be1ce46f0b792f39a6bdcccecb21b
llukyanov <leonid@lytit.com>
2015-04-12 15:56:26 -0400

    Removed bounty claim id from venue comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150412015258) do
+ActiveRecord::Schema.define(version: 20150412195517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -411,7 +411,6 @@ ActiveRecord::Schema.define(version: 20150412015258) do
     t.boolean  "from_user",            default: false
     t.integer  "session"
     t.string   "offset_created_at"
-    t.integer  "bounty_claim_id"
     t.integer  "bounty_id"
     t.boolean  "is_response"
     t.boolean  "is_response_accepted"
