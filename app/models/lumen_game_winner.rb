544297305dc656d3c89a31dfafbb112cc91b9cdc
llukyanov <leonid@lytit.com>
2015-04-14 07:42:49 -0400

    fix 5

--- app/models/lumen_game_winner.rb
+++ app/models/lumen_game_winner.rb
@@ -4,7 +4,7 @@ class LumenGameWinner < ActiveRecord::Base
   def new_winner_notification
   	message = "Congratulations #{user.name}! You are a winner in this month's Lumen Game!"
   	congratulations = Announcement.new(news: message,  title: "You Are a Winner!")
-  	message.save
+  	congratulations.save
   	recipient = AnnouncementUser.new(user_id: id, announcement_id: congratulations.id)
   	recipient.save
   	arr = []<<user
