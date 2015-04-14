9c486cf87f5e4b7cd50e7b010336d28843a02620
llukyanov <leonid@lytit.com>
2015-04-14 07:32:34 -0400

    fixes

--- app/controllers/api/v1/lumen_game_winners_controller.rb
+++ app/controllers/api/v1/lumen_game_winners_controller.rb
@@ -5,8 +5,12 @@ class Api::V1::LumenGameWinners < ApiBaseController
 		@winner.save
 		founder_1 = User.find_by_email("leonid@lytit.com")
       	founder_2 = User.find_by_name("tim@lytit.com")
-      	Mailer.delay.notify_admins_of_winner_paypal_info_provided(founder_1, @winner.user)
-      	Mailer.delay.notify_admins_of_winner_paypal_info_provided(founder_2, @winner.user)
+      	if founder_1 != nil
+      		Mailer.delay.notify_admins_of_winner_paypal_info_provided(founder_1, @winner.user)
+      	end
+      	if founder_2 != nil
+      		Mailer.delay.notify_admins_of_winner_paypal_info_provided(founder_2, @winner.user)
+		end
 		render json: { success: true }
 	end
 end
