323dd73bef85ce2334be1d80046965537113ca09
llukyanov <leonid@lytit.com>
2015-04-22 02:59:11 -0400

    hope

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -44,7 +44,7 @@ class Api::V1::UsersController < ApiBaseController
 	end
 
 	def destroy_previous_temp_user
-		previous_user = User.where("vendor_id = #{params[:vendor_id]} AND registered = FALSE").first
+		previous_user = User.where("vendor_id = ? AND registered = FALSE", params[:vendor_id]).first
 		previous_user.destroy
 		render json: { success: true }
 	end
