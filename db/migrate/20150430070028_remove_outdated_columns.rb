abba48c297053cfe5624521a9dc919c682b1d6ba
llukyanov <leonid@lytit.com>
2015-04-30 04:17:13 -0400

    reverse types added to remove column migrations

--- db/migrate/20150430070028_remove_outdated_columns.rb
+++ db/migrate/20150430070028_remove_outdated_columns.rb
@@ -1,14 +1,14 @@
 class RemoveOutdatedColumns < ActiveRecord::Migration
   def change
-  	remove_column :users, :notify_location_added_to_groups
-  	remove_column :users, :notify_events_added_to_groups
-  	remove_column :users, :notify_venue_added_to_groups
-  	remove_column :venues, :google_place_reference
-  	remove_column :venues, :google_place_rating
-  	remove_column :venues, :last_media_comment_url
-  	remove_column :venues, :last_media_comment_type
-  	remove_column :venues, :start_date
-  	remove_column :venues, :end_date
-  	remove_column :venues, :user_id 
+  	remove_column :users, :notify_location_added_to_groups, :boolean
+  	remove_column :users, :notify_events_added_to_groups, :boolean
+  	remove_column :users, :notify_venue_added_to_groups, :boolean
+  	remove_column :venues, :google_place_reference, :string
+  	remove_column :venues, :google_place_rating, :float
+  	remove_column :venues, :last_media_comment_url, :string
+  	remove_column :venues, :last_media_comment_type, :string
+  	remove_column :venues, :start_date, :datetime
+  	remove_column :venues, :end_date, :datetime
+  	remove_column :venues, :user_id, :integer
   end
 end
