56abaffc2018389bc3565153d73941dff53bdec2
llukyanov <leonid@lytit.com>
2015-05-26 18:38:33 -0400

    tweak

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -233,7 +233,7 @@ class Api::V1::VenuesController < ApiBaseController
 =end
 
 	def get_comments
-		venue_ids = params[:cluster_venue_ids].values.flatten
+		venue_ids = params[:cluster_venue_ids].split(',').map(&:to_i)
 		if not venue_ids 
 			render json: { error: { code: ERROR_NOT_FOUND, messages: ["Venue(s) not found"] } }, :status => :not_found
 		else
