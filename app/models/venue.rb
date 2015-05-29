2b4904a4917efc515f785baa6287f8145b97fe0f
llukyanov <leonid@lytit.com>
2015-05-29 11:40:35 -0400

    fix

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -798,14 +798,16 @@ class Venue < ActiveRecord::Base
 
   def self.instagram_content_pull(lat, long)
     if lat != nil && long != nil
+       
         meter_radius = 20000
         if not Venue.within(Venue.meters_to_miles(meter_radius.to_i), :origin => [lat, long]).where("rating > 0").any?
           new_instagrams = Instagram.media_search(lat, long, :distance => 5000, :count => 100)
+
           for instagram in new_instagrams
             VenueComment.convert_instagram_to_vc(instagram)
           end
+
         end
-      end
     end
   end
 
