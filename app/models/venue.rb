0eaa2a4d77024d53ba97c0656bd133a655199833
llukyanov <leonid@lytit.com>
2015-06-01 16:00:24 -0400

    fix

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -50,7 +50,7 @@ class Venue < ActiveRecord::Base
       result = false
     elsif (vname.downcase.include? "|") || (vname.downcase.include? "#") || (vname.downcase.include? ";")
       result = false
-    elsif vname.downcase.include? "snapchat" || vname.downcase.include? "whatsapp" || vname.downcase.include? "viber" || vname.downcase.include? "sms"
+    elsif (vname.downcase.include? "snapchat") || (vname.downcase.include? "whatsapp") || (vname.downcase.include? "viber") || (vname.downcase.include? "sms")
       result = false
     elsif vname != vname.titlecase
       result = false
