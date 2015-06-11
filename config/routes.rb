fec9675e79c9b2f77a14b6be01171c373fb93d3c
llukyanov <leonid@lytit.com>
2015-06-11 16:12:55 -0400

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -92,7 +92,9 @@ LytitServer::Application.routes.draw do
         get 'get_bounties'
         post 'vote'
         get 'get_area_bounty_feed'
-        get 'meta_search'
+        collection do
+          get 'meta_search'
+        end
       end
 
       controller :lytit_bar do
