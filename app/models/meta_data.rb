7073a3e83d22723efe3b20eaf75aebdd63d1a9f1
llukyanov <leonid@lytit.com>
2015-06-12 01:08:29 -0400

    remove solr leftovers

--- app/models/meta_data.rb
+++ app/models/meta_data.rb
@@ -1,15 +1,4 @@
 class MetaData < ActiveRecord::Base
 	belongs_to :venue
 	belongs_to :venue_comment
-
-	searchable do 
-		text :meta
-	end
-
-	def self.m_search(q)
-		meta_search = MetaData.search do
-			fulltext q
-		end
-		return meta_search.results
-	end
 end
