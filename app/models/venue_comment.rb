43f4f0e9efb78efc89169620c69086fd34281b20
llukyanov <leonid@lytit.com>
2015-06-12 16:38:27 -0400

    fix

--- app/models/venue_comment.rb
+++ app/models/venue_comment.rb
@@ -161,15 +161,10 @@ class VenueComment < ActiveRecord::Base
 				end	
 			end
 
-			puts '#{lytit_venue.name}----#{lytit_venue.id}'
-
 			if lytit_venue.last_instagram_pull_time == nil || (lytit_venue.last_instagram_pull_time != nil && DateTime.strptime("#{instagram.created_time}",'%s') >= lytit_venue.last_instagram_pull_time )
-				puts 'About to create a Venue Comment'
 				if not VenueComment.where("instagram_id = ?", instagram.id).any?
-					put 'Creating Venue Comment'
 					vc = VenueComment.new(:venue_id => lytit_venue.id, :media_url => instagram.images.standard_resolution.url, :media_type => "image", :content_origin => "instagram", :time_wrapper => DateTime.strptime("#{instagram.created_time}",'%s'), :instagram_id => instagram.id)
 					if vc.save
-						puts 'Venue Comment created!'
 						lytit_venue.update_columns(last_instagram_pull_time: Time.now-10.minutes)#hackery, to make sure that all instagrams of a venue in pull are not excluded after the first one
 						vote = LytitVote.new(:value => 1, :venue_id => lytit_venue.id, :user_id => nil, :venue_rating => lytit_venue.rating ? lytit_venue.rating : 0, 
 															:prime => 0.0, :raw_value => 1.0, :time_wrapper => DateTime.strptime("#{instagram.created_time}",'%s'))			
