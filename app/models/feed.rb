09e670a3101beae0b5684b98308c7fa283c3a69f
llukyanov <leonid@lytit.com>
2015-06-26 14:02:03 -0400

    fix

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,8 +4,8 @@ class Feed < ActiveRecord::Base
 	has_many :venues, through: :feed_venues
 
 	def comments
-		venue_ids = "SELECT venue_ids FROM feeds WHERE id = #{self.id}"
-		comments = VenueComment.where("venue_id IN (?) AND (NOW() - created_at) <= INTERVAL '1 DAY'", venue_ids).order("id desc")
+		venue_ids = "SELECT venue_id FROM feeds WHERE id = #{self.id}"
+		comments = VenueComment.where("venue_id IN (#{venue_ids}) AND (NOW() - created_at) <= INTERVAL '1 DAY'").order("id desc")
 	end
 
 end
