6403c507d42d884e4936381077da5b56c5781461
llukyanov <leonid@lytit.com>
2015-06-26 01:49:20 -0400

    page rank fix'

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -116,6 +116,7 @@ class Venue < ActiveRecord::Base
     view_half_life = 120.0 #minutes
     latest_page_view_time_wrapper = latest_page_view_time || Time.now
     new_page_view_count = (self.page_views * 2 ** ((-(Time.now - latest_page_view_time_wrapper)/60.0) / (view_half_life))).round(4)
+    self.update_columns(page_views: new_page_view_count)
     self.update_columns(popularity_rank: ((self.page_views+1) * self.rating))
   end
 
