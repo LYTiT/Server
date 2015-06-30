6fb08ce54a9aa00510bd7a550fa00b9c1d37f99c
llukyanov <leonid@lytit.com>
2015-06-29 22:42:02 -0400

    delayed vc to inst conversion

--- app/models/venue_comment.rb
+++ app/models/venue_comment.rb
@@ -144,7 +144,7 @@ class VenueComment < ActiveRecord::Base
 					end
 					puts "instagram venue comment created"
 					lytit_venue.feeds.update_all(new_media_present: true)
-					vc.extract_instagram_meta_data(instagram)
+					vc.delay.extract_instagram_meta_data(instagram)
 				end
 			end
 		end
