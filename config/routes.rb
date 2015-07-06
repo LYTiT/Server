70f7ad2e6161b06f90e6d087839b83a3aa2ba40f
llukyanov <leonid@lytit.com>
2015-07-06 15:05:28 -0400

    Change get comments call

--- config/routes.rb
+++ config/routes.rb
@@ -67,7 +67,7 @@ LytitServer::Application.routes.draw do
       resources :sessions, only: :create
       resources :venues, only: [:index, :show] do
         #resources :venue_ratings, only: [:create]
-        get '/posts', :action => :get_comments
+        #get '/posts', :action => :get_comments
         get '/groups', :action => :get_groups
         post '/posts/:post_id/mark_as_viewed', :action => :mark_comment_as_viewed
 
@@ -86,6 +86,9 @@ LytitServer::Application.routes.draw do
         end
         collection do
           get 'get_trending_venues'
+        end
+        collection do
+          get 'get_comments'
         end
       end
 
