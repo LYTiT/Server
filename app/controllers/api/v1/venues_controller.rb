cdb3fdd353e70896d7a51868c5a6bf4c3e250ebb
llukyanov <leonid@lytit.com>
2015-07-25 15:50:31 -0400

    Commenting

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -322,6 +322,7 @@ class Api::V1::VenuesController < ApiBaseController
 	end
 
 	def get_contexts
+		#Hanlding both for individual venue and cluster
 		if params[:cluster_venue_ids] != nil
 			venue_ids = params[:cluster_venue_ids].split(',').map(&:to_i)
 			@contexts = MetaData.where("venue_id IN (?)", venue_ids).order("relevance_score DESC LIMIT 5")
