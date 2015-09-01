41cfeb1d6d494677ff909363aaa3a0687fd6419e
llukyanov <leonid@lytit.com>
2015-08-31 21:07:01 -0400

    order change

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -110,7 +110,7 @@ class Api::V1::FeedsController < ApiBaseController
 	end
 
 	def get_chat
-		@messages = FeedMessage.where("feed_id = ?", params[:feed_id]).order("id DESC")
+		@messages = FeedMessage.where("feed_id = ?", params[:feed_id]).order("id asc")
 	end
 
 	def meta_search
