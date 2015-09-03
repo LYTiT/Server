d0252888bd2351fdf7ce8c62bc2f58f37f9c715c
llukyanov <leonid@lytit.com>
2015-09-02 22:59:40 -0400

    no delay

--- app/models/feed_message.rb
+++ app/models/feed_message.rb
@@ -25,7 +25,7 @@ class FeedMessage < ActiveRecord::Base
 		    :user_phone => user.phone,
 		    :feed_id => feed.id,
 		    :feed_name => feed.name,
-		    :chat_message => self.message,
+		    :chat_message => self.message
 
 		}
 
