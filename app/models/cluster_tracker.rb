4668147c3d3a593782a7509efaf076d8a6083ec3
llukyanov <leonid@lytit.com>
2015-09-09 20:41:00 -0400

    changes

--- app/models/cluster_tracker.rb
+++ app/models/cluster_tracker.rb
@@ -3,9 +3,9 @@ class ClusterTracker < ActiveRecord::Base
 	def self.check_existence(lat, long, zoom)
 		result = ClusterTracker.where("latitude = ? AND longitude = ? AND zoom_level = ?", lat, long, zoom).first
 		if result == nil
-			ClusterTracker.create!(:latitude => lat, :longitude => long, :zoom_level => zoom)
+			new_cluster_tracker = ClusterTracker.create!(:latitude => lat, :longitude => long, :zoom_level => zoom)
 		else
-			result
+			return result
 		end
 	end
 end
