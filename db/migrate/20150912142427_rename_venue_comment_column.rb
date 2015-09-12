620e963d4f3d4f4c07f30e9f6efc51ea8fe8437f
llukyanov <leonid@lytit.com>
2015-09-12 10:45:49 -0400

    fixes

--- db/migrate/20150912142427_rename_venue_comment_column.rb
+++ db/migrate/20150912142427_rename_venue_comment_column.rb
@@ -1,5 +1,5 @@
 class RenameVenueCommentColumn < ActiveRecord::Migration
   def change
-  	rename_column :venue_comments, :image_url_1, :image_url_1
+  	rename_column :venue_comments, :media_url, :image_url_1
   end
 end
