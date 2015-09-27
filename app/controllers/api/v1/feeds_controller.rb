d3b537dca5db9acd45235c0e3d86045892002360
llukyanov <leonid@lytit.com>
2015-09-27 01:42:19 -0400

    rescue

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -135,7 +135,8 @@ class Api::V1::FeedsController < ApiBaseController
 	def send_message
 		feed_ids = params[:feed_ids].split(',').map(&:to_i)
 		if feed_ids.count == 1
-			new_message = FeedMessage.create!(:message => params[:chat_message], :feed_id => feed_ids.first, :user_id => params[:user_id], :venue_comment_id => params[:venue_comment_id])
+			vc_id = 
+			new_message = FeedMessage.create!(:message => params[:chat_message], :feed_id => feed_ids.first, :user_id => params[:user_id], :venue_comment_id => params[:venue_comment_id]) rescue FeedMessage.create!(:message => params[:chat_message], :feed_id => feed_ids.first, :user_id => params[:user_id], :venue_comment_id => nil)
 			render json: { success: true }
 		else
 			for feed_id in feed_ids
