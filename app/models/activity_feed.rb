3350e297840d672a1b03f2f917f4104ca58e4fea
llukyanov <leonid@lytit.com>
2015-11-03 07:43:42 -0500

    Fix

--- app/models/activity_feed.rb
+++ app/models/activity_feed.rb
@@ -2,13 +2,8 @@ class ActivityFeed < ActiveRecord::Base
 	belongs_to :feed
 	belongs_to :activity
 
-	def self.populate
-		total_activity = Activity.all
-		total_activity.each{|activity| ActivityFeed.create!(:activity_id => activity.id, :feed_id => activity.feed_id)}
-	end
-
 	def self.bulk_creation(target_activity_id, f_ids)
-		feed_ids.each{|f_id| ActivityFeed.create!(:activity_id => target_activity_id, :feed_id => f_id)}
+		f_ids.each{|f_id| ActivityFeed.create!(:activity_id => target_activity_id, :feed_id => f_id)}
 	end
 
 end
