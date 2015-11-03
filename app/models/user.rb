9d826ba2cf756431d24fc62231156922948f333e
llukyanov <leonid@lytit.com>
2015-11-03 03:06:20 -0500

    new aggregate feed structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -439,8 +439,10 @@ class User < ActiveRecord::Base
   #IV. Lists
 
   def aggregate_list_feed
-    user_feed_ids = "SELECT feed_id from feed_users WHERE user_id = #{self.id}"
-    Activity.where("feed_id IN (#{user_feed_ids}) AND (NOW() - created_at) <= INTERVAL '1 DAY' AND adjusted_sort_position IS NOT NULL").includes(:user, :venue, :venue_comment, :feed, :feed_share, :likes).order("adjusted_sort_position DESC")    
+    user_feed_ids = "SELECT feed_id FROM feed_users WHERE user_id = #{self.id}"
+    #Activity.where("feed_id IN (#{user_feed_ids}) AND (NOW() - created_at) <= INTERVAL '1 DAY' AND adjusted_sort_position IS NOT NULL").includes(:user, :venue, :venue_comment, :feed, :feed_share, :likes).order("adjusted_sort_position DESC")    
+    activity_ids = "SELECT activity_id FROM activity_feeds WHERE feed_id IN (#{user_feed_ids})"
+    Activity.where("id IN (#{activity_ids}) AND (NOW() - created_at) <= INTERVAL '1 DAY' AND adjusted_sort_position IS NOT NULL").includes(:user, :venue, :venue_comment, :feed, :feed_share, :likes).order("adjusted_sort_position DESC")
   end
 
   #------------------------------------------------------------->
