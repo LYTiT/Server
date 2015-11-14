5b11095d6ac39c63d6a4aaf136caa30a4dd16a4b
llukyanov <leonid@lytit.com>
2015-11-13 20:52:42 -0500

    post revert

--- app/controllers/api/v1/announcement_controller.rb
+++ app/controllers/api/v1/announcement_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::AccesscodesController < ApiBaseController
+class Api::V1::AnnouncementsController < ApiBaseController
 
 	def get_announcement_details
 		@announcement = Announcement.find_by_id(params[:announcement_id])
