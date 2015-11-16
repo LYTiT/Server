7abb9123734f0fe7282bd666945d650037f5bae1
llukyanov <leonid@lytit.com>
2015-11-16 03:24:51 -0500

    special map

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::EventsController < ApiBaseController
 	end
 
 	def get_announcements
-		@event_announcements = Event.find_by_id(params[:event_id]).event_announcements.order("id DESC").page(params[:page]).per(10)
+		@event_announcements =  EventAnnouncement.where("event_id = ?", params[:event_id]).order("id DESC").page(params[:page]).per(10)
 	end
 
 	def create_event
