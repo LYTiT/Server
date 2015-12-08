6a7c46c4f6e723d973123116d16a99c9b5b35cb9
llukyanov <leonid@lytit.com>
2015-12-08 00:12:00 -0500

    tweaks

--- app/models/venue_question_comment.rb
+++ app/models/venue_question_comment.rb
@@ -5,7 +5,7 @@ class VenueQuestionComment < ActiveRecord::Base
 
 	def self.new_comment(v_q_id, comment, v_id, u_id, comment_user_on_location)
 		new_venue_question_message = VenueQuestionComment.create!(:venue_question_id => v_q_id, :user_id => u_id, :comment => comment)
-		venue_question.increment(:num_comments, 1)
+		new_venue_question_message.venue_question.increment(:num_comments, 1)
 		new_venue_question_message.new_question_message_notification(user_on_location)
 	end
 	
