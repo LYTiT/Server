d2d88fac534a557c363a6a455d0c6d67d05b0b40
llukyanov <leonid@lytit.com>
2015-12-10 04:41:09 -0500

    no touch scroll attempt 3

--- app/assets/javascripts/no_touch_scroll.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($){
-	//target the entire page, and listen for touch events
-	$('html, body').on('touchmove', function(e){ 
-	     //prevent native touch activity like scrolling
-	     e.preventDefault(); 
-	});
-});
