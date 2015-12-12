6d6f1b75683b085a7005c9fde0323f1ac6e0c7e0
llukyanov <leonid@lytit.com>
2015-12-11 23:00:44 -0500

    tweak

--- app/models/live_user.rb
+++ app/models/live_user.rb
@@ -4,17 +4,15 @@ class LiveUser < ActiveRecord::Base
 
 	after_create :make_venue_live
 
-	def make_venue_live
-		self.venue.update_columns(is_live: true)
-	end
-
 	def LiveUser.cleanup
 		live_users = LiveUser.where("created_at < ?" Time.now-45.minutes).includes(:venue)
-
 		for live_user in live_users
 			live_user.venue.update_columns(is_live: false)
 			live_user.delete
 		end
 	end
 
+	def make_venue_live
+		self.venue.update_columns(is_live: true)
+	end
 end
