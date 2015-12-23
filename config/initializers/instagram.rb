1d2c160daf33ee42b93eae8861686e911dff3db4
llukyanov <leonid@lytit.com>
2015-12-23 04:15:30 -0500

    new instagram access token

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,4 +1,4 @@
 Instagram.configure do |config|
 	config.client_id = 'a810f7f4e0f740fc99ae9fadce166215'
-	config.access_token = '1518251367.1677ed0.43e1adbcf5a249a1b128031ab08e4bc3'
+	config.access_token = '1518251367.1677ed0.7350f8d30acb45ed9df69814badf6be2'
 end
