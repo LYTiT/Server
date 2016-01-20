531fe13fe98cee4357beea0f5583d712804e00d5
llukyanov <leonid@lytit.com>
2016-01-20 11:46:17 -0500

    tweak

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -295,7 +295,6 @@ class Api::V1::VenuesController < ApiBaseController
 		end
 =end		
 
-
 		if params[:page].to_i == 1
 			cache_key = "lyt_map_by_parts/[#{lat.to_f.round(2)},#{long.to_f.round(2)}]_near"
 			nearby_venues = Rails.cache.fetch(cache_key, :expires_in => 5.minutes) do
@@ -307,11 +306,10 @@ class Api::V1::VenuesController < ApiBaseController
 			faraway_venues = Rails.cache.fetch(cache_key, :expires_in => 5.minutes) do
 				Venue.where("(color_rating > -1.0 OR is_live IS TRUE) AND ((latitude < #{proximity_box.sw.lat} OR latitude > #{proximity_box.ne.lat}) AND (longitude < #{proximity_box.sw.lng} OR longitude > #{proximity_box.ne.lng}))")
 			end
-			@venues = faraway_venues.page(params[:page].to_i-1).per(num_page_entries)
+			@venues = faraway_venues.page(params[:page]).per(num_page_entries)
 		end
 
 		@view_cache_key = cache_key+"/[#{lat.to_f.round(2)},#{long.to_f.round(2)}]/part_"+params[:page]
-		#@venues = lit_venues.page(params[:page]).per(num_page_entries)
 		render 'display_by_parts.json.jbuilder'
 	end
 
