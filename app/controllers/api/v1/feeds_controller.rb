6dcc449d2ac851715850720c2a82444d3ce00115
llukyanov <leonid@lytit.com>
2016-01-27 13:40:24 -0500

    tweaks' :

--- app/controllers/api/v1/feeds_controller.rb
+++ app/controllers/api/v1/feeds_controller.rb
@@ -153,7 +153,7 @@ class Api::V1::FeedsController < ApiBaseController
 	def get_feed
 		@user = User.find_by_authentication_token(params[:auth_token])
 		@feed = Feed.find_by_id(params[:feed_id])		
-		@feed.delay.update_comments
+		@feed.delay.update_underlying_venues
 	end
 
 	def get_activity
