3cf658ab7fb5314527a57818d9367f8d9270deb6
llukyanov <leonid@lytit.com>
2016-02-05 13:20:18 -0500

    tweaks

--- app/models/feed_venue.rb
+++ app/models/feed_venue.rb
@@ -14,8 +14,13 @@ class FeedVenue < ActiveRecord::Base
 	end
 
 	def new_venue_notification_and_activity
+		if user == nil
+			user = feed.user
+		end
+		
 		a = Activity.create!(:feed_id => feed_id, :feed_name => feed.name, :feed_color => feed.feed_color, :activity_type => "added_venue", :feed_venue_id => self.id, 
-			:user_id => user_id, :user_name => user.name, :user_phone => user.phone_number, :venue_id => venue_id, :venue_name => venue.name, 
+			:user_id => self.user_id, :user_name => user.name, :user_phone => user.phone_number, :user_facebook_id => user.facebook_id, :user_facebook_name => user.facebook_name,
+			:venue_id => venue_id, :venue_name => venue.name, 
 			:venue_instagram_location_id => venue.instagram_location_id, :venue_latitude => venue.latitude,
 			:venue_longitude => venue.longitude, :venue_address => venue.address, :venue_city => venue.city,
 			:venue_state => venue.state, :venue_country => venue.country, :venue_added_note => self.description,
