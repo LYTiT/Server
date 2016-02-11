248372a492c5c132fbddd88f50a8fd80ebd6e804
llukyanov <leonid@lytit.com>
2016-02-11 17:25:27 -0500

    Tweak

--- db/migrate/20160211205444_add_instagram_id_to_venues.rb
+++ db/migrate/20160211205444_add_instagram_id_to_venues.rb
@@ -1,6 +1,6 @@
 class AddInstagramIdToVenues < ActiveRecord::Migration
   def change
   	add_column :venues, :venue_comment_instagram_id, :string
-  	
+  	add_column :venues, :venue_comment_instagram_user_id, :integer, :limit => 8
   end
 end
