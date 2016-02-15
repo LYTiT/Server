b04eda2a452844addddcaaface167705efd6e84d
llukyanov <leonid@lytit.com>
2016-02-15 15:11:21 -0500

    tweak

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -730,12 +730,12 @@ class Venue < ActiveRecord::Base
       Instagram.location_recent_media(self.instagram_location_id, :min_timestamp => (Time.now-24.hours).to_time.to_i)
     else
       if self.last_instagram_post != nil
-        Instagram.location_recent_media(self.instagram_location_id, :min_id => self.last_instagram_post) rescue nil
+        Instagram.location_recent_media(self.instagram_location_id, :min_id => self.last_instagram_post) rescue []
       else
         if hourly_pull == true
-          Instagram.location_recent_media(self.instagram_location_id, :min_timestamp => (Time.now-1.hour).to_time.to_i) rescue nil
+          Instagram.location_recent_media(self.instagram_location_id, :min_timestamp => (Time.now-1.hour).to_time.to_i) rescue []
         else
-          Instagram.location_recent_media(self.instagram_location_id, :min_timestamp => (Time.now-24.hours).to_time.to_i) rescue nil
+          Instagram.location_recent_media(self.instagram_location_id, :min_timestamp => (Time.now-24.hours).to_time.to_i) rescue []
         end
       end
     end
