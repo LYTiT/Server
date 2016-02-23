797bf66d449ff0094bf11316f5d643f46bf977a4
llukyanov <leonid@lytit.com>
2016-02-23 12:00:00 -0500

    tweak

--- app/assets/javascripts/image_swap.js
+++ app/assets/javascripts/image_swap.js
@@ -3,9 +3,10 @@ jQuery(document).ready(function($){
 	$(function() {
 	  if($(window).width() <= 1024) {
 	    $("img").each(function() {
-	      $(this).attr("src", $(this).attr("src").replace("/assets/lists@2x.png", "/assets/lists_m@2x.png"));
-	      $(this).attr("src", $(this).attr("src").replace("/assets/pages@2x.png", "/assets/pages_m@2x.png"));
-	      $(this).attr("src", $(this).attr("src").replace("/assets/shake@2x.png", "/assets/shake_m@2x.png"));
+			$(this).attr("src", $(this).attr("src").replace("/assets/list@2x.png", "/assets/list_m@2x.png"));
+			$(this).attr("src", $(this).attr("src").replace("/assets/find_list@2x.png", "/assets/find_list_m@2x.png"));
+			$(this).attr("src", $(this).attr("src").replace("/assets/place_search@2x.png", "/assets/place_search_m@2x.png"));
+			$(this).attr("src", $(this).attr("src").replace("/assets/live_search@2x.png", "/assets/live_search_m@2x.png"));
 	    });
 	  }
 	});
