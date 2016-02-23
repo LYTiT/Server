8317a40f2da9e8bc65085cb349116d19140fa3ea
llukyanov <leonid@lytit.com>
2016-02-23 01:55:27 -0500

    Fix

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -801,7 +801,7 @@ class Venue < ActiveRecord::Base
     if day_pull == true
       Instagram.location_recent_media(self.instagram_location_id, :min_timestamp => (Time.now-24.hours).to_time.to_i)
     else
-      if self.last_instagram_post != nil
+      if self.last_instagram_post != nil && hourly_pull == false
         Instagram.location_recent_media(self.instagram_location_id, :min_id => self.last_instagram_post).map(&:to_hash) rescue []
       else
         if hourly_pull == true
