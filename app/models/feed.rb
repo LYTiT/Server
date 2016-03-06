95bd30a13734a03e2157b05f02fd94f65e4bd529
llukyanov <leonid@lytit.com>
2016-03-05 19:31:42 -0500

    search tweak

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -70,11 +70,11 @@ class Feed < ActiveRecord::Base
 	def Feed.lookup(query)
 		if (query =~ /\d/) != nil #we check if query contains a number in it. If it does we 
 			#have to do an explicit lookup since pg:search struggles with digits for some reason.
-			direct_match_ids = "SELECT id FROM feeds WHERE LOWER(name) = #{query.downcase}"
-			Feed.robust_search(query).where("id NOT IN (#{direct_match_ids})")
+			direct_match_ids = "SELECT id FROM feeds WHERE LOWER(name) = ('#{query.downcase}')"
+			Feed.robust_search(query).where("id NOT IN (#{direct_match_ids})").limit(10)
 			search_results = Feed.robust_search(query).with_pg_search_rank
 			top_search_results = search_results.select { |venue| venue.pg_search_rank >= 0.2 }
-			return Feed.where("id in (#{direct_match_ids})").limit(5)+top_search_results.limit(10)
+			return Feed.where("id in (#{direct_match_ids})").limit(5)+top_search_results
 		else
 			search_results = Feed.robust_search(query).with_pg_search_rank.limit(15)
 			top_search_results = search_results.select { |venue| venue.pg_search_rank >= 0.2 }
