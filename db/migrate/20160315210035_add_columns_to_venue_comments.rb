d826798b6ced3c5a30367233f048f196f4e83fa0
llukyanov <leonid@lytit.com>
2016-03-15 17:29:06 -0400

    tweak

--- db/migrate/20160315210035_add_columns_to_venue_comments.rb
+++ db/migrate/20160315210035_add_columns_to_venue_comments.rb
@@ -1,6 +1,5 @@
 class AddColumnsToVenueComments < ActiveRecord::Migration
   def change
-  	add_column :venue_comments, :num_views, :integer
   	add_column :venue_comments, :geo_views, :json, default: {}, null: false
   end
 end
