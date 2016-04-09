3d07bf3cd1760eafc39593a6d532e8b17d2eb47e
llukyanov <leonid@lytit.com>
2016-04-08 23:42:34 -0400

    tweaks

--- app/models/event.rb
+++ app/models/event.rb
@@ -36,7 +36,7 @@ class Event < ActiveRecord::Base
 				:start_time => eventbrite_event.start.utc.to_datetime, :end_time => eventbrite_event.end.utc.to_datetime, :source_url => eventbrite_event.url, 
 				:source => "Eventbrite", :venue_id => venue.id, :category => eventbrite_event.category.try(:name), :cover_image_url => eventbrite_event.logo.try(:url))
 			VenueComment.create!(:entry_type => "event", :venue_id => venue.id, :venue_details => venue.partial, :event => new_event.partial, :adjusted_sort_position => (new_event.start_time+1.hour).to_i)
-			venue.update_columns(event_id: new_event.id)
+			venue.update_columns(event_details: new_event.partial)
 		else
 			p "Event not created."
 		end
