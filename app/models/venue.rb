8442d82f3cb611f4eaaab6287bef42f5153bbf3d
llukyanov <leonid@lytit.com>
2016-04-11 18:44:56 -0400

    Tweak

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -667,7 +667,7 @@ class Venue < ActiveRecord::Base
         self.increment!(:page_offset, new_super_content_num_pages)
         self.venue_comments.where("adjusted_sort_position >= ?", current_position).order("adjusted_sort_position DESC").limit(page_count).to_a
       else
-        if (self.last_instagram_pull_time == nil or self.last_twitter_pull_time == nil) or ((Time.now - api_ping_timeout) > self.last_instagram_pull_time or (Time.now - api_ping_timeout) > self.last_twitter_pull_time)
+        if ((self.last_instagram_pull_time == nil or self.last_twitter_pull_time == nil) or ((Time.now - api_ping_timeout) > self.last_instagram_pull_time or (Time.now - api_ping_timeout) > self.last_twitter_pull_time))
           new_social_media = self.live_social_media_search
           new_social_media_num_pages = new_social_media.count/page_count + (new_social_media.count%page_count != 0 ? 1:0)
           if new_social_media_num_pages > 0
@@ -690,7 +690,7 @@ class Venue < ActiveRecord::Base
           end
         else
           #The page offset value is the amount of proceeding pages filled with either super content or live social media.
-          vcs = self.venue_comments.where("adjusted_sort_position < ?", current_position).limit(page_count).offset(((page_number-self.page_offset)-1)*page_count).order("adjusted_sort_position DESC")
+          vcs = self.venue_comments.where("adjusted_sort_position < ? AND created_at <= ?", current_position, Time.now-10.minutes).limit(page_count).offset(((page_number-self.page_offset)-1)*page_count).order("adjusted_sort_position DESC")
           if vcs.count > 0
             vcs.to_a
           else
