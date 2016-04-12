6f5266a054083dd620c1cf6d050e46604ab2ad03
llukyanov <leonid@lytit.com>
2016-04-11 23:24:32 -0400

    tweak

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -364,7 +364,7 @@ class Api::V1::UsersController < ApiBaseController
 	def get_happening_venue_recs
 		user = User.find_by_authentication_token(params[:auth_token])
 		#@venues = Venue.where("color_rating > 0").limit(15)
-		@venues = Venue.live_recommendation_for(user, params[:latitdue], params[:longitude])
+		@venues = Venue.live_recommendation_for(user, params[:latitude], params[:longitude])
 	end
 
 	#-------------------------------------------------->
