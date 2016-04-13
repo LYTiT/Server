28343867ce27dbd70dd31ab47cbe44614a0bae32
llukyanov <leonid@lytit.com>
2016-04-13 16:47:52 -0400

    fix

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1932,7 +1932,11 @@ end
     new_venue_tweets = client.search(query+" -rt", result_type: "recent", geocode: "#{latitude},#{longitude},#{radius}km", since_id: "#{min_tweet_id}").take(20).collect.to_a
     self.update_columns(last_twitter_pull_time: Time.now)
 
-    new_venue_tweets.take new_venue_tweets.size-1
+    if new_tweets.length > 0
+      new_venue_tweets.take new_venue_tweets.length-1
+    else
+      []
+    end
   end
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
