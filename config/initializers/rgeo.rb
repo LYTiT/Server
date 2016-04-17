c591e0b6561c257c45ae91dfd096c7ed0c72f25f
llukyanov <leonid@lytit.com>
2016-04-17 13:21:03 -0400

    rgeo setup tweak

--- config/initializers/rgeo.rb
+++ config/initializers/rgeo.rb
@@ -1,7 +1,13 @@
-RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
-  # By default, use the GEOS implementation for spatial columns.
-  config.default = RGeo::Geos.factory_generator
+=begin
+	RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
+	  # By default, use the GEOS implementation for spatial columns.
+	  config.default = RGeo::Geos.factory_generator
 
-  # But use a geographic implementation for point columns.
-  config.register(RGeo::Geographic.spherical_factory(srid: 4326), geo_type: "point")
-end
+	  # But use a geographic implementation for point columns.
+	  config.register(RGeo::Geographic.spherical_factory(srid: 4326), geo_type: "point")
+	end
+=end
+
+RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
+  config.default = RGeo::Geographic.spherical_factory(srid: 4326)
+end	
