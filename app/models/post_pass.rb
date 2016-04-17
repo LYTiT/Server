9a0f31760342a29a8fdc306da6fa3095b097e466
llukyanov <leonid@lytit.com>
2016-04-17 19:35:18 -0400

    fix

--- app/models/post_pass.rb
+++ app/models/post_pass.rb
@@ -27,38 +27,37 @@ class PostPass < ActiveRecord::Base
 	def send_new_post_pass_notification
   		vc = self.venue_comment
 		vc_user = vc.user
-			if vc.views == 1 || (vc.views%5 == 0 && vc.views <= 20) || (vc.views%10 && vc.views > 20)
-				payload = {
-					:object_id => self.id,       
-					:type => 'post_pass_notification',
-					:id => vc.id,
-					:media_type => vc.lytit_post["media_type"],
-					:media_dimensions => vc.lytit_post["media_dimensions"],
-					:image_url_1 => vc.lytit_post["image_url_1"],
-					:image_url_2 => vc.lytit_post["image_url_2"],
-					:image_url_3 => vc.lytit_post["image_url_3"],
-					:video_url_1 => vc.lytit_post["video_url_1"],
-					:video_url_2 => vc.lytit_post["video_url_2"],
-					:video_url_3 => vc.lytit_post["video_url_3"],
-					:venue_id => vc.venue_details["id"],
-					:venue_name => vc.venue_details["name"],
-					:latitude => vc.venue_details["latitdue"],
-					:longitude => vc.venue_details["longitude"],
-					:timestamp => vc.created_at,
-					:content_origin => 'lytit',
-					:geo_views => vc.geo_views,
-					:num_views => vc.views
-				}
+		if vc.views == 1 || (vc.views%5 == 0 && vc.views <= 20) || (vc.views%10 && vc.views > 20)
+			payload = {
+				:object_id => self.id,       
+				:type => 'post_pass_notification',
+				:id => vc.id,
+				:media_type => vc.lytit_post["media_type"],
+				:media_dimensions => vc.lytit_post["media_dimensions"],
+				:image_url_1 => vc.lytit_post["image_url_1"],
+				:image_url_2 => vc.lytit_post["image_url_2"],
+				:image_url_3 => vc.lytit_post["image_url_3"],
+				:video_url_1 => vc.lytit_post["video_url_1"],
+				:video_url_2 => vc.lytit_post["video_url_2"],
+				:video_url_3 => vc.lytit_post["video_url_3"],
+				:venue_id => vc.venue_details["id"],
+				:venue_name => vc.venue_details["name"],
+				:latitude => vc.venue_details["latitdue"],
+				:longitude => vc.venue_details["longitude"],
+				:timestamp => vc.created_at,
+				:content_origin => 'lytit',
+				:geo_views => vc.geo_views,
+				:num_views => vc.views
+			}
 
-				type = "post_pass/#{vc.id}/#{vc.views}"
+			type = "post_pass/#{vc.id}/#{vc.views}"
 
-				notification = self.store_new_notification(payload, vc_user, type)
-				payload[:notification_id] = notification.id
+			notification = self.store_new_notification(payload, vc_user, type)
+			payload[:notification_id] = notification.id
 
-				if vc_user.push_token && vc_user.active == true
-					count = Notification.where(user_id: vc_user.id, read: false, deleted: false).count
-					APNS.send_notification(vc_user.push_token, { :priority =>10, :alert => preview, :content_available => 1, :other => payload, :badge => count})
-				end
+			if vc_user.push_token && vc_user.active == true
+				count = Notification.where(user_id: vc_user.id, read: false, deleted: false).count
+				APNS.send_notification(vc_user.push_token, { :priority =>10, :alert => preview, :content_available => 1, :other => payload, :badge => count})
 			end
 		end
 	end
