19ca80598389663adf1d4c879c8a15d62fee46e2
llukyanov <leonid@lytit.com>
2016-05-12 18:47:05 -0400

    Tweak

--- app/models/list_category.rb
+++ app/models/list_category.rb
@@ -2,12 +2,14 @@ class ListCategory < ActiveRecord::Base
 	has_many :list_category_entries
 
 	def ListCategory.populate
-		rec_categories = FeedRecommendation.where("category IS NOT NULL").pluck(:category)
+		rec_categories = FeedRecommendation.where("category IS NOT NULL").pluck(:category).uniq
 		for category in rec_categories
-			lc = ListCategory.create!(:name => category)
+			lc = ListCategory.create!(:name => category, :thumbnail_image_url => "cat_"+category.downcase)
 			category_feeds = FeedRecommendation.where("category = ?", category)
 			category_feeds.each{|feed| ListCategoryEntry.create!(:feed_id => feed.id, :list_category_id => lc.id)}
 		end
 	end
+
+
 
 end
