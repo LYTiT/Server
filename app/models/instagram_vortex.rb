633c86b859811111be1b9cbfb1d209ad65cba227
llukyanov <leonid@lytit.com>
2016-05-16 15:33:55 -0400

    fix

--- app/models/instagram_vortex.rb
+++ app/models/instagram_vortex.rb
@@ -23,7 +23,7 @@ class InstagramVortex < ActiveRecord::Base
 	has_many :vortex_paths, :dependent => :destroy
 
 	def InstagramVortex.global_pull
-      vortexes = InstagramVortex.where("active = ?")
+      vortexes = InstagramVortex.where("active IS TRUE")
       for vortex in vortexes
         puts "Entered vortex #{vortex.details}"
         vortex.pull
