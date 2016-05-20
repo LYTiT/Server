40f435bef2eb8743c2cade27aca43d556293fa52
llukyanov <leonid@lytit.com>
2016-05-19 23:35:39 -0400

    tweak

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1806,7 +1806,11 @@ def Venue.live_recommendation_for(user, lat=40.741140, long=-73.981917)
     results = Venue.in_bounds(search_box).order("popularity_rank DESC").interest_search(interest_query).where("popularity_rank > 0.0").limit(30)
 
     if results.length < 30
-      results << Venue.in_bounds(search_box).order("popularity_rank DESC").where("color_rating > -1.0").limit(30-results.length)
+      if results.length == 0
+        Venue.in_bounds(search_box).order("popularity_rank DESC").where("color_rating > -1.0").limit(30-results.length)
+      else
+        results += Venue.in_bounds(search_box).order("popularity_rank DESC").where("color_rating > -1.0").limit(30-results.length)
+      end
     end
   else
     results = Venue.in_bounds(search_box).order("popularity_rank DESC").where("color_rating > -1.0").limit(30)  
