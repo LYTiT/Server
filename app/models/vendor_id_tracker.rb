95924bf50ffc6246af1acb7781b5bd3d72e1a0ef
llukyanov <leonid@lytit.com>
2016-05-23 15:28:12 -0400

    user creation tweak

--- app/models/vendor_id_tracker.rb
+++ app/models/vendor_id_tracker.rb
@@ -2,7 +2,7 @@ class VendorIdTracker < ActiveRecord::Base
 
 	def VendorIdTracker.implicit_creation(u_id)
 		user = User.find_by_id(u_id)
-		if user != nil and VendorIdTracker.where("LOWER(used_vendor_id) = ?", user.vendor_id.downcase).first.nil? == true
+		if user != nil and user.vendor_id.downcase != nil and VendorIdTracker.where("LOWER(used_vendor_id) = ?", user.vendor_id.downcase).first.nil? == true
   			VendorIdTracker.create!(:used_vendor_id => u_id)
   		end
 	end
