1f3a24e42fb30f12b48330c1f3c3c879df495a4f
llukyanov <leonid@lytit.com>
2016-05-26 23:37:42 -0400

    tweak

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -28,7 +28,8 @@ class Feed < ActiveRecord::Base
 				}
 			},
 			:query => query,
-			:ranked_by => "(:tsearch * (1 + (floor(0.5-:tsearch)+1) * 1/(ST_Distance(central_mass_lonlat_geography, ST_GeographyFromText('SRID=4326;POINT(#{latitude} #{longitude})'))/1000.0)))",
+			:ranked_by => "CASE :tsearch < 0.5 WHEN TRUE THEN :tsearch * 1/(ST_Distance(central_mass_lonlat_geography, ST_GeographyFromText('SRID=4326;POINT(#{latitude} #{longitude})'))*1000.0) ELSE :tsearch END",
+			#"(:tsearch * ((floor(0.5-:tsearch)+1) + (floor(0.5-:tsearch)+1) * 1/(ST_Distance(central_mass_lonlat_geography, ST_GeographyFromText('SRID=4326;POINT(#{latitude} #{longitude})'))/1000.0)))",
 			:order_within_rank => "central_mass_lonlat_geometry <-> st_point(#{longitude},#{latitude})"			
 		}
 	}
